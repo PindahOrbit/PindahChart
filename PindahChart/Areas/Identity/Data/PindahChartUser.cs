260be8f747eeeda2493adcbe7ebb220fd5a4fbe5
Leeroy-Mubaiwa <markmubaiwa@gmail.com>
2022-07-13 16:27:25 +0200

    First Commit

---
+++ PindahChart/Areas/Identity/Data/PindahChartUser.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace PindahChart.Areas.Identity.Data
+{
+    // Add profile data for application users by adding properties to the PindahChartUser class
+    public class PindahChartUser : IdentityUser
+    {
+        [Required]
+        public string FullName { get; set; }
+    }
+}
---
+++ PindahChart/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using PindahChart.Areas.Identity.Data;
+using PindahChart.Data;
+
+[assembly: HostingStartup(typeof(PindahChart.Areas.Identity.IdentityHostingStartup))]
+namespace PindahChart.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<PindahChartIdentityContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("PindahChartIdentityContextConnection")));
+
+                services.AddDefaultIdentity<PindahChartUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<PindahChartIdentityContext>();
+            });
+        }
+    }
+}
